363485c394d69dfa986c7419b77236fdf8e22b62
uzakotim <timauzakov@gmail.com>
2025-02-05 13:45:47 +0500

    nojekyll

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enables static export
-    basePath: "/uzakotim.github.io", // GitHub Pages serves from a subpath
-    assetPrefix: "/uzakotim.github.io",
+    basePath: "", // GitHub Pages serves from a subpath
     images: {
       unoptimized: true, // Required if using Next.js images
     },
